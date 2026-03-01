6e6e746f67c69a6cd507297be61299d0fe34c4ce
nachiketa <nachiketasharma@gmail.com>
2026-03-01 22:39:00 +0530

    Coded UC3 to use string join

--- Program.java
+++ Program.java
@@ -1,9 +1,9 @@
 public class Program{
     public static void main(String[] args){
-        System.out.println("*****"+" "+"*****"+" "+"*****"+" "+"******");
-        System.out.println("*   *"+" "+"*   *"+" "+"*   *"+" "+"*     ");
-        System.out.println("*   *"+" "+"*   *"+" "+"*****"+" "+"******");
-        System.out.println("*   *"+" "+"*   *"+" "+"*    "+" "+"     *");
-        System.out.println("*****"+" "+"*****"+" "+"*    "+" "+"******");
+        System.out.println(String.join(" ","*****","*****","*****","******"));
+        System.out.println(String.join(" ","*   *","*   *","*   *","*     "));
+        System.out.println(String.join(" ","*   *","*   *","*****","******"));
+        System.out.println(String.join(" ","*   *","*   *","*    ","     *"));
+        System.out.println(String.join(" ","*****","*****","*    ","******"));
     }
 }
