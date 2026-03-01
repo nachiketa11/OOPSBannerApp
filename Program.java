3ac1fc6717286fa801b7d9f4ed63fb1718656cd5
nachiketa <nachiketasharma@gmail.com>
2026-03-01 22:50:32 +0530

    Coded UC6 to use static functions

--- Program.java
+++ Program.java
@@ -1,15 +1,39 @@
 public class Program{
-    public static void main(String[] args){
-        String[] lines = {
-            String.join(" ","*****","*****","*****","******"),
-            String.join(" ","*   *","*   *","*   *","*     "),
-            String.join(" ","*   *","*   *","*****","******"),
-            String.join(" ","*   *","*   *","*    ","     *"),
-            String.join(" ","*****","*****","*    ","******")    
+    public static String[] getOPattern(){
+        return new String[]{
+            "*****",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*****"
         };
+    }
+    public static String[] getPPattern(){
+        return new String[]{
+            "*****",
+            "*   *",
+            "*****",
+            "*    ",
+            "*    "
+        };
+    }
+    public static String[] getSPattern(){
+        return new String[]{
+            "******",
+            "*     ",
+            "******",
+            "     *",
+            "******"
+        };
+    }
+    public static void main(String[] args){
+        
+        String[] OPattern = getOPattern();
+        String[] PPattern = getPPattern();
+        String[] SPattern = getSPattern();
 
-        for (String line : lines){
-            System.out.println(line);
+        for (int i=0;i<OPattern.length;i++){
+            System.out.println(OPattern[i]+" "+OPattern[i]+" "+PPattern[i]+" "+SPattern[i]);
         }
     }
 
